6eb8f698074d12225bd62a5dca25e8bf1946e61e
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-10-14 16:28:48 -0300

    Teste do REST

---
+++ src/main/java/com/lacsystem/pedido/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package com.lacsystem.pedido.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Luiz.Cesario
+ *
+ */
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest funcionando";
+	}
+
+}
