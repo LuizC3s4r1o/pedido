5e411a758e98395efa51b7015f8eb0ab6b479e73
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-10-15 23:35:03 -0300

    Implementacao de Cidade e Estado

--- src/main/java/com/lacsystem/pedido/PedidoApplication.java
+++ src/main/java/com/lacsystem/pedido/PedidoApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lacsystem.pedido.domain.Categoria;
+import com.lacsystem.pedido.domain.Cidade;
+import com.lacsystem.pedido.domain.Estado;
 import com.lacsystem.pedido.domain.Produto;
 import com.lacsystem.pedido.repositories.CategoriaRepository;
+import com.lacsystem.pedido.repositories.CidadeRepository;
+import com.lacsystem.pedido.repositories.EstadoRepository;
 import com.lacsystem.pedido.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class PedidoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoApplication.class, args);
@@ -40,13 +50,25 @@ public class PedidoApplication implements CommandLineRunner {
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
-		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Urbelâmdia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 		
 	}
 }
