bcd5a472411e2d1f41a4530e6286b59f0400d2b4
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-11-01 23:43:08 -0300

    Listando categorias com DTO

--- src/main/java/com/lacsystem/pedido/resources/CategoriaResource.java
+++ src/main/java/com/lacsystem/pedido/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.lacsystem.pedido.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lacsystem.pedido.domain.Categoria;
+import com.lacsystem.pedido.dto.CategoriaDTO;
 import com.lacsystem.pedido.services.CategoriaService;
 
 /**
@@ -51,7 +54,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
