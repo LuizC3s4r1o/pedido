fe2d25c5b65f15320a832f87456729c59088f5d7
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-11-01 23:21:59 -0300

    Deletando categoria - METHOD DELETE

--- src/main/java/com/lacsystem/pedido/services/CategoriaService.java
+++ src/main/java/com/lacsystem/pedido/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.lacsystem.pedido.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lacsystem.pedido.domain.Categoria;
 import com.lacsystem.pedido.repositories.CategoriaRepository;
+import com.lacsystem.pedido.services.exceptions.DataIntegrityException;
 import com.lacsystem.pedido.services.exceptions.ObjectNotFoundException;
 
 /**
@@ -34,6 +36,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Long id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
---
+++ src/main/java/com/lacsystem/pedido/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lacsystem.pedido.services.exceptions;
+
+/**
+ * @author Luiz.Cesario
+ *
+ */
+public class DataIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable causa) {
+		super(msg, causa);
+	}
+
+}
