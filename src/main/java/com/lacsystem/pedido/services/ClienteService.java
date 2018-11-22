afde0a02027e22275c5a4746ad26381384742ba5
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-11-21 23:42:47 -0200

    Usando padrao de nomes para imagens

--- src/main/java/com/lacsystem/pedido/services/ClienteService.java
+++ src/main/java/com/lacsystem/pedido/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.lacsystem.pedido.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -47,6 +49,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+		
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Long id) {
 		UserSpringSecurity user = UserService.authenticated();
@@ -121,13 +129,11 @@ public class ClienteService {
 		if(user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		URI uri = s3Service.uploadFile(multipartFile);
 		
-		Cliente cliente = this.find(user.getId());
-		cliente.setImageUrl(uri.toString());
-		repo.save(cliente);
-		
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		 
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 
 }
